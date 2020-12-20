cfcb93772ab7625767a9487d3daf71a6176ab064
ayavuz2 <a.yavuzum@gmail.com>
2020-12-20 03:56:52 +0300

    Added backtrack algorithm. It works perfect.

--- solver.py
+++ solver.py
@@ -1,15 +1,40 @@
 class Spot:
-    def __init__(self, row, col, val):
+    def __init__(self, row, col, val, board):
         self.row = row
         self.col = col
         self.val = val
-        self.set = {}
-        self.latin_square = (self.row//3, self.col//3) # n×n array filled with n different symbols/numbers
+        self.board = board
+        self.set = None
+        self.latin_square = ((self.row//3)*3, (self.col//3)*3) # n×n array filled with n different symbols/numbers
 
-    def set_valid_val(self, B):
+    def change_val(self, value):
+        self.temp_val = self.val
+        self.val = value
+        self.board[self.row][self.col] = self.val
+
+    def set_valid_val(self):
         A = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-        self.set = A.difference(B)
-        print(self.set)
+
+        unvalid_set = set()
+
+        # adding the values that is on same col
+        for i in range(len(self.board)):
+            unvalid_set.add(self.board[i][self.col])
+
+        # adding the values that is on same row
+        for j in range(len(self.board[self.row])):
+            unvalid_set.add(self.board[self.row][j])
+
+        # adding the values that is in the same Latin Square
+        x, y = self.return_Lsquare_range()
+        for i in range(x, x+3):
+            for j in range(y, y+3):
+                unvalid_set.add(self.board[i][j])
+
+        self.set = A.difference(unvalid_set)
+        # print(self.set)
+
+        return True if len(self.set) != 0 else False
 
     def return_Lsquare_range(self):
         return self.latin_square
@@ -29,7 +54,14 @@ class Grid:
     ]
 
     def __init__(self):
-        self.bo = [[Spot(i, j, self.board[i][j]) for j in range(len(self.board[0]))] for i in range(len(self.board))]
+        self.bo = [[Spot(i, j, self.board[i][j], self.board) for j in range(len(self.board[0]))] for i in range(len(self.board))]
+
+    def find_empty(self):
+        for i in range(len(self.bo)):
+            for j in range(len(self.bo[i])):
+                if self.bo[i][j].val == 0:
+                    return self.bo[i][j]
+        return None
 
     def print_board(self):
         for row in range(len(self.bo)):
@@ -37,26 +69,24 @@ class Grid:
                 print(self.bo[row][col].val, end=' ')
             print("")
 
-    def find_unvalid(self, row, col):
-        unvalid_set = set()
-        print(type(unvalid_set))
-        # adding the values that is on same col
-        for i in range(len(self.bo)):
-            unvalid_set.add(self.bo[i][col].val)
 
-        # adding the values that is on same row
-        for j in range(len(self.bo[row])):
-            unvalid_set.add(self.bo[row][j].val)
+def backtrack(spot):
+    if spot == None:
+        print("Its done :) \n")
+        return True
 
-        # adding the values that is in the same Latin Square
-        x, y = self.bo[row][col].return_Lsquare_range()
-        for i, j in zip(range(x, x+3), range(y, y+3)):
-            unvalid_set.add(self.bo[i][j].val)
+    if spot.set_valid_val():
+        while len(spot.set) != 0:
+            spot.change_val(spot.set.pop())
+            if backtrack(g.find_empty()) == True:
+                return True
+            else:
+                spot.change_val(0)
 
-        self.bo[row][col].set_valid_val(unvalid_set)
-
-
+    return False
+    
 
 g = Grid()
-# g.print_board()
-g.find_unvalid(1, 1)
+
+if backtrack(g.find_empty()):
+    g.print_board()
